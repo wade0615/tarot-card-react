1b1f6c77460f85cec69243735c2c8d49835fe042
Wade <>
2021-12-23 11:44:38 +0800

    feat: sample.js

---
+++ src/layouts/sample.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function sample() {
+  const Sample = styled.section`
+    position: relative;
+    color: #878787;
+  `
+  return (
+    <Sample>
+      Sample
+    </Sample>
+  );
+}
+
+export default sample;
