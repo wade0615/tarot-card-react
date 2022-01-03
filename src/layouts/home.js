3af04c5967d044413db670db5e9e7d6f2302cfd4
Wade <>
2022-01-03 15:38:06 +0800

    feat: getACard()

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import CardArrayRadioGroup from "../components/CardArrayRadioGroup";
 import SingleCardResult from "../components/SingleCardResult";
 
+import cards from "../assets/cards/index";
+
 import Button from '@mui/material/Button';
 
 const HomeDiv = styled.div`
@@ -11,11 +13,22 @@ const HomeDiv = styled.div`
 `
 
 function Home() {
+  function getRangeRandom(min,max) {
+    return Math.floor(Math.random()*(max-min+1))+min;
+  };
+  function getACard() {
+    console.log("getACard");
+    const firstCardIndex = getRangeRandom(0, cards.length - 1);
+    console.log("firstCardIndex", firstCardIndex, cards[firstCardIndex].name);
+  };
+
   return (
     <HomeDiv className="h-screen flex justify-center items-center">
       <div>
         <CardArrayRadioGroup />
-        <Button variant="outlined">抽卡</Button>
+        <Button variant="outlined" onClick={getACard}>
+          抽卡
+        </Button>
         <SingleCardResult name="Bob" />
       </div>
     </HomeDiv>
